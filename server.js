3fe19f5de58d56c35fcd880cfe36bda146dc8fa3
fidoandroid <rahulkumarrk2247@gmail.com>
2017-03-16 16:21:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/circular', function (req, res) {
+app.get('/circular', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'circular.html'));
 });
 
