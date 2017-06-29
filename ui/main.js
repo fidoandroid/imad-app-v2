223d82ecaa9ba2e703c95f5c8f5695ae012464fa
fidoandroid <rahulkumarrk2247@gmail.com>
2017-06-29 09:52:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,18 @@ console.log('Loaded!');
 //change text of h1
 var ele=document.getElementByClassName("center");
 ele.innerHtml="GEC BANDA";
+
+
+
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "ajax_info.txt", true);
+  xhttp.send();
+}
